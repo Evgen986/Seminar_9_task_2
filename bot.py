05a848d9cff50907e5c44be4cc1893bb9e21d88f
Evgen986 <Evgen986@mail.ru>
2023-01-28 12:57:26 +0300

    token

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import bot_commands
 
 
-app = ApplicationBuilder().token("5841865362:AAGe-mekNEv9gYIPrV8kxAA6eNihfpSGJrU").build()
+app = ApplicationBuilder().token("Введите токен бота").build()
 print('Сервер запустился')
 
 app.add_handler(CommandHandler("g_1", bot_commands.g_1))
